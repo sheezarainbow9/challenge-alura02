823b9492c8c5521f2cd4766a5a66c5c0ed5024c3
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-04-05 10:41:39 -0300

    js

--- validate.js
+++ validate.js
@@ -9,40 +9,41 @@ botaoEnviar.addEventListener("click", function (event) {
   var error = document.querySelector("#error");
   error.style.color = "black";
   var form = document.querySelector("form");
-
-  function validarDados() {
-    var msgError = [];
-
-    if (document.form.nome.value == "") {
-      msgError.push("Preenchimento obrigatório!");
-      document.form.nome.focus();
-    } else if (document.form.email.value == "") {
-      msgError.push("Preenchimento obrigatório!");
-      document.form.email.focus();
-    } else if (document.form.assunto.value == "") {
-      msgError.push("Preenchimento obrigatório!");
-      document.form.assunto.focus();
-    } else if (
-      document.form.textarea.value == "" ||
-      document.form.textarea.value.length < 50
-    ) {
-      msgError.push(
-        "Campo Mensagem é obrigatório e deve conter pelo menos 50 caracteres."
-      );
-      document.form.textarea.focus();
-    } else if (
-      document.form.email.value.indexOf("@") == -1 ||
-      document.form.email.value.indexOf(".") == -1
-    ) {
-      msgError.push("E-mail inválido!");
-    }
-
-    error.innerHTML = msgError.join(",");
-  }
-  document.querySelector("form").addEventListener("submit", validarDados);
-
-  form.reset();
 });
+
+function validarDados() {
+  var msgError = [];
+
+  if (document.form.nome.value == "") {
+    msgError.push("Preenchimento obrigatório!");
+    document.form.nome.focus();
+  } else if (document.form.email.value == "") {
+    msgError.push("Preenchimento obrigatório!");
+    document.form.email.focus();
+  } else if (document.form.assunto.value == "") {
+    msgError.push("Preenchimento obrigatório!");
+    document.form.assunto.focus();
+  } else if (
+    document.form.textarea.value == "" ||
+    document.form.textarea.value.length < 50
+  ) {
+    msgError.push(
+      "Campo Mensagem é obrigatório e deve conter pelo menos 50 caracteres."
+    );
+    document.form.textarea.focus();
+  } else if (
+    document.form.email.value.indexOf("@") == -1 ||
+    document.form.email.value.indexOf(".") == -1
+  ) {
+    msgError.push("E-mail inválido!");
+  }
+
+  error.innerHTML = msgError.join(",");
+}
+
+document.querySelector("form").addEventListener("submit", validarDados);
+
+form.reset();
 
 // function validarDados() {
 //   event.preventDefault();
